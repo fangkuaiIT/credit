a7d09f5d275051615688ef7b9cce02cd19430f97
- <->
2020-04-06 23:52:22 +0800

    second

---
+++ src/test/java/com/fangkuai/credit/CreditApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fangkuai.credit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreditApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
